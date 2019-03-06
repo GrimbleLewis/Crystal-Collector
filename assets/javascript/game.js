506c0b1061367a6ec39e56484d98abfe60dec950
Grimble <grimblelewisthethird@gmail.com>
2019-03-06 00:10:22 -0500

    converted html to bootstrap and finished layout. Added the wins and loss and reset functions as well as appending wins and losses to the html. Still need to create the onclick functions for each of the crystals.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,21 +3,49 @@ $(document).ready(function() {
 //global variables
 
 // the target number the player is trying to match
-var random = Math.floor(Math.random()*101 + 19);
+var random = Math.floor(Math.random() * 101 + 19);
 console.log(random);
 
 // random numbers for each crystal
-var num1 = Math.floor(Math.random()*11 + 1);
-var num2 = Math.floor(Math.random()*11 + 1);
-var num3 = Math.floor(Math.random()*11 + 1);
-var num4 = Math.floor(Math.random()*11 + 1);
+var num1 = Math.floor(Math.random() * 11 + 1);
+var num2 = Math.floor(Math.random() * 11 + 1);
+var num3 = Math.floor(Math.random() * 11 + 1);
+var num4 = Math.floor(Math.random() * 11 + 1);
 
 var total = 0;
 var wins = 0;
 var losses = 0;
 
 
-$("#targetNumber").append(random);
+$("#target-number").append(random);
+$("#wins-counter").append(wins);
+$("#losses-counter").append(losses);
+
+function resetGame(){
+    random = Math.floor(Math.random() * 101 + 19);
+    console.log(random);
+    $("#target-number").append(random);
+    num1 = Math.floor(Math.random() * 11 + 1);
+    num2 = Math.floor(Math.random() * 11 + 1);
+    num3 = Math.floor(Math.random() * 11 + 1);
+    num4 = Math.floor(Math.random() * 11 + 1);
+    total = 0;
+    $("#user-total").append(total);
+}
+
+function win(){
+    alert("Winner winner chicken dinner!");
+    wins++;
+    $("#wins-counter").append(wins);
+    resetGame();
+}
+
+function loss(){
+    alert("Overconfidence is a slow and insidious killer, loser!");
+    loses++;
+    $("#losses-counter").append(losses);
+    resetGame();
+}
 
 
 
