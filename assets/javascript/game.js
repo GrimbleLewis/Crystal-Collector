cc0c622b072737e361c056c2c09d080e2cd64df0
Grimble <grimblelewisthethird@gmail.com>
2019-03-08 15:55:39 -0500

    added the on click functions for crystals, changed append to text to prevent the numbers from not replacing the numbers/text and added some text to the html to prevent text from moving around with wins/losses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
 
 // the target number the player is trying to match
 var random = Math.floor(Math.random() * 101 + 19);
-console.log(random);
 
 // random numbers for each crystal
 var num1 = Math.floor(Math.random() * 11 + 1);
@@ -17,37 +16,74 @@ var wins = 0;
 var losses = 0;
 
 
-$("#target-number").append(random);
-$("#wins-counter").append(wins);
-$("#losses-counter").append(losses);
+$("#target-number").text(random);
+$("#wins-counter").text("Wins: " + wins);
+$("#losses-counter").text("Losses: " + losses);
 
 function resetGame(){
     random = Math.floor(Math.random() * 101 + 19);
-    console.log(random);
-    $("#target-number").append(random);
+    $("#target-number").text(random);
     num1 = Math.floor(Math.random() * 11 + 1);
     num2 = Math.floor(Math.random() * 11 + 1);
     num3 = Math.floor(Math.random() * 11 + 1);
     num4 = Math.floor(Math.random() * 11 + 1);
     total = 0;
-    $("#user-total").append(total);
+    $("#user-total").text(total);
 }
 
 function win(){
     alert("Winner winner chicken dinner!");
     wins++;
-    $("#wins-counter").append(wins);
+    $("#wins-counter").text("Wins: " + wins);
     resetGame();
 }
 
 function loss(){
     alert("Overconfidence is a slow and insidious killer, loser!");
-    loses++;
-    $("#losses-counter").append(losses);
+    losses++;
+    $("#losses-counter").text("Losses: " + losses);
     resetGame();
 }
 
+$("#red").on("click", function(){
+    total += num1;
+    $("#user-total").text(total);
+    if (total === random){
+        win();
+    } else if (total > random){
+        loss();
+    }
+});
 
+$("#green").on("click", function(){
+    total += num2;
+    $("#user-total").text(total);
+    if (total === random){
+        win();
+    } else if (total > random){
+        loss();
+    }
+});
+
+$("#blue").on("click", function(){
+    total += num3;
+    $("#user-total").text(total);
+    if (total === random){
+        win();
+    } else if (total > random){
+        loss();
+    }
+});
+
+$("#pink").on("click", function(){
+    total += num4;
+    $("#user-total").text(total);
+    if (total === random){
+        win();
+    } else if (total > random){
+        loss();
+    }
+});
 
 
 });
